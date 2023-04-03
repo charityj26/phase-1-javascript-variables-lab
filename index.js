0a3a0807e89d3ee3912e93e590b26d21b203569e
charity <charity.joseph@students.moringaschool.com>
2023-04-03 12:48:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-
 // Code your solution in this file!
-const companyName = "Scuber";
-let mostProfitableNeighborhood = "Chelsea";
-let companyCeo = "Susan Smith";
+function scuberGreetingForFeet(ride){
+    //write your code here
+    if (ride <=400){
+        return `This one is on me!`;
+    }else if(ride >=2500){
+        return "No can do.";
+    }else {
+        return " I will gladly take your thirty bucks.";
+    }
+    debugger
+
+}
+function ternaryCheckCity(city){
+    //write your code here
+    if (city === "NYC") {
+        return `ok,sounds good.`;
+    }else if (city!=="NYC"){
+        return "No go.";
+    }
+}
+
+function switchOnCharmFromTip(tip){
+    //write your code here!
+    if(tip === "generous"){
+        return`Thankyou so much`;
+    }else if(tip === "not as generous"){
+        return `Thankyou`;
+    }else{
+        retun `Bye`;
+    }
+    //switch (tip) {
+        // case "Tip":
+        //"Thamk you so much.";
+    }
